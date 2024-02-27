9efe629e94b06078c2f67d3544d1d0bc1820f338
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 10:20:53 +0500

    Add files via upload

---
+++ Alien_colors_2.ts
@@ -0,0 +1,17 @@
+// Version 1 (Runs the if block)
+let alien_color_if = 'green';
+
+if (alien_color_if === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else {
+    console.log("Congratulations! You just earned 10 points.");
+}
+
+// Version 2 (Runs the else block)
+let alien_color_else = 'red'; // Change to any color other than 'green' to run the else block
+
+if (alien_color_else === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else {
+    console.log("Congratulations! You just earned 10 points.");
+}
